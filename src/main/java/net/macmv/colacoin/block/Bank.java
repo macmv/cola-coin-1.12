2be3f4cc6804d2dadaf257d3853d6b865f7dd122
Neil Macneale V <neil.macneale.v@gmail.com>
2024-07-24 23:13:21 -0700

    Add a basic GUI for text input

--- src/main/java/net/macmv/colacoin/block/Bank.java
+++ src/main/java/net/macmv/colacoin/block/Bank.java
@@ -1,12 +1,18 @@
 package net.macmv.colacoin.block;
 
 import net.macmv.colacoin.Utils;
+import net.macmv.colacoin.gui.LoginScreen;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class Bank extends Block {
   private static final AxisAlignedBB BOUNDING_BOX = Utils.aabb(1, 0, 1, 15, 16, 15);
@@ -23,5 +29,16 @@ public class Bank extends Block {
   @Override
   public boolean isOpaqueCube(IBlockState state) {
     return false;
+  }
+
+  @Override
+  public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+    // All the interaction is client-side only.
+    if (!worldIn.isRemote) {
+      return true;
+    }
+
+    Minecraft.getMinecraft().displayGuiScreen(new LoginScreen());
+    return true;
   }
 }
---
+++ src/main/java/net/macmv/colacoin/gui/LoginScreen.java
@@ -0,0 +1,65 @@
+package net.macmv.colacoin.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.io.IOException;
+
+@SideOnly(Side.CLIENT)
+public class LoginScreen extends GuiScreen {
+  private static final ResourceLocation LOGIN_BACKGROUND = new ResourceLocation("colacoin:textures/gui/login_background.png");
+
+  protected final int windowWidth = 176;
+  protected final int windowHeight = 166;
+  private int x;
+  private int y;
+  private String secret = "";
+
+  @Override
+  public void initGui() {
+    this.x = (this.width - this.windowWidth) / 2;
+    this.y = (this.height - this.windowHeight) / 2;
+  }
+
+  @Override
+  public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+    this.drawDefaultBackground();
+
+    GlStateManager.pushMatrix();
+    GlStateManager.translate(x, y, 0);
+
+    GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+    mc.getTextureManager().bindTexture(LOGIN_BACKGROUND);
+    drawTexturedModalRect(0, 0, 0, 0, this.windowWidth, this.windowHeight);
+
+    int width = fontRenderer.getStringWidth(secret);
+    drawString(fontRenderer, secret, 34, 53, 0xffffff);
+    drawRect(34 + width, 52, 35 + width, 53 + fontRenderer.FONT_HEIGHT, 0xff000000);
+
+    GlStateManager.popMatrix();
+  }
+
+  @Override
+  protected void keyTyped(char typed, int keyCode) throws IOException {
+    super.keyTyped(typed, keyCode);
+
+    if ((typed >= 'a' && typed <= 'z') || (typed >= 'A' && typed <= 'Z') || (typed >= '0' && typed <= '9') || typed == '_' || typed == '-') {
+      secret += typed;
+    }
+
+    if (typed == '\b' && !secret.isEmpty()) {
+      secret = secret.substring(0, secret.length() - 1);
+    }
+
+    if (typed == 22 /* Ctrl+V */) {
+      secret += GuiScreen.getClipboardString();
+    }
+
+    if (secret.length() > 40) {
+      secret = secret.substring(0, 40);
+    }
+  }
+}
